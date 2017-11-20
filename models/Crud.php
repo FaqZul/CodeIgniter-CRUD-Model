b24542ea3e35b28e7ec73e5cf264cc1a9d9ce431
FaqZul <faqzul@gmail.com>
2017-11-21 00:10:30 +0700

    Add custom WHERE

--- models/Crud.php
+++ models/Crud.php
@@ -1,68 +1,102 @@
 <?php
 /**
-* @author 		Muhammad Faqih Zulfikar
-* @copyright 	Copyright (c) 2017 FaqZul (https://github.com/FaqZul/CodeIgniter-CRUD-Model)
-* @license 		https://opensource.org/licenses/MIT 	MIT License
-* @link 		https://www.facebook.com/DorkSQLi
-* @package		FaqZul/CodeIgniter-CRUD-Model
-* @subpackage	Models
-* @version 		3.0.0
-*/
+ * @author		Muhammad Faqih Zulfikar
+ * @copyright	Copyright (c) 2017 FaqZul (https://github.com/FaqZul/CodeIgniter-CRUD-Model)
+ * @license		https://opensource.org/licenses/MIT 	MIT License
+ * @link		https://www.facebook.com/DorkSQLi
+ * @package		FaqZul/CodeIgniter-CRUD-Model
+ * @subpackage	Models
+ * @version		3.1.0
+ */
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Crud extends CI_Model {
 
-	private $_error = array('code' => 0, 'message' => '');
+	/**
+	 * Last error
+	 *
+	 * @var array
+	 */
+	protected $_error = array('code' => 0, 'message' => '');
 
-	private $insert_id_val = 0;
+	/**
+	 * Insert ID
+	 *
+	 * @var int
+	 */
+	protected $insert_id_val = 0;
 
-	/*
-	* Delete Record (Soft Delete)
-	* Data will be deleted permanently if the value is TRUE.
-	* To save Your data but not to display, set it to FALSE & add the following fields in each table:
-	* 	[TABLENAME]_delete_date	datetime 	DEFAULT NULL;
-	* 	[TABLENAME]_delete_ip	varchar(15)	DEFAULT NULL;
-	*/
+	/**
+	 * Delete Record (Soft Delete)
+	 * Data will be deleted permanently if the value is TRUE.
+	 * To save Your data but not to display, set it to FALSE & add the following fields in each table:
+	 * 	[TABLENAME]_delete_date	datetime 	DEFAULT NULL;
+	 * 	[TABLENAME]_delete_ip	varchar(15)	DEFAULT NULL;
+	 *
+	 * @var bool
+	 */
 	protected $delete_record = TRUE;
 
-	/*
-	* The insert ID number when performing database inserts.
-	* If using the PDO driver with PostgreSQL, or using the Interbase driver, $this->crud->insert_id() function requires a $insert_id_key parameter, which specifies the appropriate sequence to check for the insert id.
-	*/
+	/**
+	 * The insert ID number when performing database inserts.
+	 * If using the PDO driver with PostgreSQL, or using the Interbase driver, $this->crud->insert_id() function requires a $insert_id_key parameter, which specifies the appropriate sequence to check for the insert id.
+	 *
+	 * @var string
+	 */
 	protected $insert_id_key = NULL;
 
-	/*
-	* Save the last query that was run.
-	* If the value is TRUE, run the following query in Your database:
-	* CREATE TABLE `log` (
-	* 	`log_id` int(11) NOT NULL AUTO_INCREMENT,
-	* 	`log_ip` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '127.0.0.1',
-	* 	`log_query` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-	* 	`log_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '127.0.0.1',
-	* 	`log_datetime` datetime NOT NULL,
-	* 	PRIMARY KEY (`log_id`) USING BTREE
-	* ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-	*/
+	/**
+	 * Save the last query that was run.
+	 * If the value is TRUE, run the following query in Your database:
+	 * CREATE TABLE `log` (
+	 * 	`log_id` int(11) NOT NULL AUTO_INCREMENT,
+	 * 	`log_ip` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '127.0.0.1',
+	 * 	`log_query` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+	 * 	`log_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '127.0.0.1',
+	 * 	`log_datetime` datetime NOT NULL,
+	 * 	PRIMARY KEY (`log_id`) USING BTREE
+	 * ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+	 *
+	 * @var bool
+	 */
 	protected $log_query = FALSE;
 
-	/*
-	* History Transaction
-	* If the value is TRUE, add the following fields in each table:
-	* 1. For inserting data:
-	* 	[TABLENAME]_create_date	datetime 	DEFAULT NULL;
-	* 	[TABLENAME]_create_ip	varchar(15)	DEFAULT NULL;
-	* 2. For updating data:
-	* 	[TABLENAME]_update_date	datetime 	DEFAULT NULL;
-	* 	[TABLENAME]_update_ip	varchar(15)	DEFAULT NULL;
-	*/
+	/**
+	 * History Transaction
+	 * If the value is TRUE, add the following fields in each table:
+	 * 1. For inserting data:
+	 * 	[TABLENAME]_create_date	datetime 	DEFAULT NULL;
+	 * 	[TABLENAME]_create_ip	varchar(15)	DEFAULT NULL;
+	 * 2. For updating data:
+	 * 	[TABLENAME]_update_date	datetime 	DEFAULT NULL;
+	 * 	[TABLENAME]_update_ip	varchar(15)	DEFAULT NULL;
+	 *
+	 * @var bool
+	 */
 	protected $track_trans = FALSE;
 
+	protected $wheres = array(
+		'where', 'or_where', 'or_where_in', 'or_where_not_in', 'where_in', 'where_not_in', 'like', 'or_like', 'not_like', 'or_not_like',
+		'where_esc', 'or_where_esc', 'or_where_in_esc', 'or_where_not_in_esc', 'where_in_esc', 'where_not_in_esc', 'like_esc', 'or_like_esc', 'not_like_esc', 'or_not_like_esc'
+	);
+
+	/**
+	 * Class Constructor
+	 *
+	 * @return void
+	 */
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper('crud');
 		if (file_exists(APPPATH . 'config/crud.php') AND $this->config->load('crud', TRUE)) { $this->initialize($this->config->item('crud')); }
 	}
 
+	/**
+	 * Initialize Preferences
+	 *
+	 * @param	array	$config
+	 * @return	Crud
+	 */
 	public function initialize(array $config = array()) {
 		foreach ($config as $key => $val) {
 			if (isset($this->$key)) { $this->$key = $val; }
@@ -70,6 +104,14 @@ class Crud extends CI_Model {
 		return $this;
 	}
 
+	/**
+	 * Create Data
+	 *
+	 * @param	string	$table
+	 * @param	array	$data
+	 * @param	bool	$callback
+	 * @return	mixed
+	 */
 	public function createData($table, $data, $callback = FALSE) {
 		if (is_array_assoc($data) AND $this->track_trans === TRUE) {
 			$data[$table . '_create_date'] = date('Y-m-d H:i:s');
@@ -87,13 +129,25 @@ class Crud extends CI_Model {
 		else { return ($this->error_message() !== '') ? FALSE: TRUE; }
 	}
 
+	/**
+	 * Read Data
+	 *
+	 * @param	string	$select
+	 * @param	mixed	$from
+	 * @param	mixed	$wheres
+	 * @param	array	$joinTable
+	 * @param	mixed	$groupBy
+	 * @param	string	$orderBy
+	 * @param	array	$limit
+	 * @return	mixed
+	 */
 	public function readData($select, $from, $wheres = NULL, $joinTable = NULL, $groupBy = NULL, $orderBy = NULL, $limit = NULL) {
 		$this->db->select($select, FALSE);
 		$this->db->from($from);
 		if (is_array_assoc($joinTable)) {
 			foreach ($joinTable as $k => $v) { $this->db->join($k, $v); }
 		}
-		if (is_array_assoc($wheres)) { $this->db->where($wheres); }
+		if (is_array_assoc($wheres)) { $this->set_wheres($wheres); }
 		else if (is_string($wheres) AND trim($wheres) !== '') { $this->db->where($wheres); }
 		if ($this->delete_record === FALSE) { $this->db->where($from . '_delete_date', NULL); }
 		if (is_array($groupBy)) { $this->db->group_by($groupBy); }
@@ -110,6 +164,15 @@ class Crud extends CI_Model {
 		return ($this->error_message() !== '') ? FALSE: $query;
 	}
 
+	/**
+	 * Update Data
+	 *
+	 * @param	string	$table
+	 * @param	array	$data
+	 * @param	mixed	$wheres
+	 * @param	bool	$callback
+	 * @return	mixed
+	 */
 	public function updateData($table, $data, $wheres, $callback = FALSE) {
 		if (is_array_assoc($data) AND $this->track_trans === TRUE) {
 			$data[$table . '_update_date'] = date('Y-m-d H:i:s');
@@ -125,12 +188,20 @@ class Crud extends CI_Model {
 		else { return ($this->error_message() !== '') ? FALSE: TRUE; }
 	}
 
+	/**
+	 * Delete Data
+	 *
+	 * @param	string	$table
+	 * @param	mixed	$wheres
+	 * @param	bool	$callback
+	 * @return	mixed
+	 */
 	public function deleteData($table, $wheres, $callback = FALSE) {
-		if (is_array_assoc($data) AND $this->delete_record === FALSE) {
+		if ($this->delete_record === FALSE) {
 			$data[$table . '_delete_date'] = date('Y-m-d H:i:s');
 			$data[$table . '_delete_ip'] = $this->input->ip_address();
 		}
-		if (is_array_assoc($wheres)) { $this->db->where($wheres); }
+		if (is_array_assoc($wheres)) { $this->set_wheres($wheres); }
 		else if (is_string($wheres) AND trim($wheres) !== '') { $this->db->where($wheres); }
 		($this->delete_record === FALSE) ? $this->db->update($table, $data): $this->db->delete($table);
 		$this->set_error($this->db->error());
@@ -140,21 +211,121 @@ class Crud extends CI_Model {
 		else { return ($this->error_message() !== '') ? FALSE: TRUE; }
 	}
 
+	/**
+	 * Error
+	 *
+	 * Returns an array containing code and message of the last
+	 * database error that has occurred.
+	 *
+	 * @return array
+	 */
 	public function error() { return $this->_error; }
 
+	/**
+	 * The error message number
+	 *
+	 * @return int
+	 */
 	public function error_code() { return $this->_error['code']; }
 
+	/**
+	 * The error message string
+	 *
+	 * @return string
+	 */
 	public function error_message() { return $this->_error['message']; }
 
+	/**
+	 * Insert ID
+	 *
+	 * @return int
+	 */
 	public function insert_id() { return $this->insert_id_val; }
 
-	private function set_insert_id($var = 0) {
+	/**
+	 * Set Insert ID
+	 *
+	 * @param	int 	$var
+	 * @return	Crud
+	 */
+	protected function set_insert_id($var = 0) {
 		$this->insert_id_val = (int) $var;
 		return $this; 
 	}
 
+	/**
+	 * Generates the WHERE portion of the query
+	 *
+	 * @param	array 	$arr
+	 * @return	void
+	 */
+	protected function set_wheres($arr = array()) {
+		if (in_array_assoc($this->wheres, $arr)) {
+			foreach ($arr as $keys => $vals) {
+				if (in_array($keys, $this->wheres)) {
+					if (strpos($keys, '_esc') !== FALSE) {
+						$key = str_replace('_esc', '', $keys);
+						if (is_array_assoc($vals)) {
+							foreach ($vals as $k => $v) { $this->db->$key($k, $v, FALSE); }
+						}
+						else if (is_array_multi($vals)) {
+							foreach ($vals as $val) {
+								if (is_array($val)) {
+									foreach ($val as $k => $v) { $this->db->$key($k, $v, FALSE); }
+								}
+								else if (is_string($val)) { $this->db->$key($val, NULL, FALSE); }
+							}
+						}
+						else if (is_string($vals)) { $this->db->$key($vals, NULL, FALSE); }
+					}
+					else {
+						if (is_array_assoc($vals)) {
+							foreach ($vals as $k => $v) { $this->db->$keys($k, $v); }
+						}
+						else if (is_array_multi($vals)) {
+							foreach ($vals as $val) { $this->db->$keys($val); }
+						}
+						else if (is_string($vals)) { $this->db->$keys($vals); }
+					}
+					/* $key = str_replace('_esc', '', $keys);
+					if (is_array_assoc($vals)) {
+						foreach ($vals as $k => $v) { $this->db->$key($k, $v, ( ! isset("$keys_esc"))); }
+					}
+					else if (is_array_multi($vals)) {
+						if (strpos($keys, '_esc') === FALSE) {
+							foreach ($vals as $val) { $this->db->$key($val); }
+						}
+						else {
+							foreach ($vals as $val) {
+								if (is_array($val)) {
+									foreach ($val as $k => $v) { $this->db->$key($k, $v, FALSE); }
+								}
+								else if (is_string($val)) { $this->db->$key($val, NULL, FALSE); }
+							}
+						}
+					}
+					else if (is_string($vals)) { (strpos($keys, '_esc') === FALSE) ? $this->db->$key($vals): $this->db->$key($vals, NULL, FALSE); } */
+				}
+				else { $this->db->where($keys, $vals); }
+			}
+		}
+		else { $this->db->where($arr); }
+	}
+
+	/**
+	 * Log Queries
+	 *
+	 * @param	string	$var
+	 * @return	Crud
+	 */
 	protected function log($var = '') { if (trim($var) !== '') { $this->db->insert('log', array('log_ip' => $this->input->ip_address(), 'log_query' => $var, 'log_url' => ( ! isset($_SERVER['REDIRECT_URL'])) ? base_url(): $_SERVER['REDIRECT_URL'], 'log_datetime' => date('Y-m-d H:i:s'))); } }
 
+	/**
+	 * Set Error
+	 *
+	 * @param	array	$var
+	 * @return	Crud
+	 */
 	protected function set_error($var = array()) {
 		if (is_array_assoc($var) AND isset($var['code']) AND isset($var['message'])) {
 			$this->_error['code'] = (int) $var['code'];
