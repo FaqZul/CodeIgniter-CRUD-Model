e74e4dbee1abb5d6eb50b71e67993e974e61a9d5
FaqZul <faqzul@gmail.com>
2017-11-20 22:09:01 +0700

    Add function in_array_assoc

--- helpers/crud_helper.php
+++ helpers/crud_helper.php
@@ -10,6 +10,32 @@
 */
 defined('BASEPATH') or exit('No direct script access allowed');
 
+/**
+ * Checks if a value exists in an array
+ *
+ * @param	mixed	$needle
+ * @param	array	$haystack
+ * @return	bool
+ */
+if ( ! function_exists('in_array_assoc')) {
+	function in_array_assoc($needle, $haystack, $strict = FALSE) {
+		$return = FALSE;
+		foreach ($haystack as $k => $v) {
+			if (in_array($k, $needle, $strict)) {
+				$return = TRUE;
+				break;
+			}
+		}
+		return $return;
+	}
+}
+
+/**
+ * Finds whether a variable is an associative arrays
+ *
+ * @param	array	$arr
+ * @return	bool
+ */
 if ( ! function_exists('is_array_assoc')) {
-	function is_array_assoc($var) { return is_array($var) && array_diff_key($var, array_keys(array_keys($var))); }
+	function is_array_assoc($arr = array()) { return is_array($arr) && array_diff_key($arr, array_keys(array_keys($arr))); }
 }
